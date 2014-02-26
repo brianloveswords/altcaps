ed309c6cb703407b5ddb4db6eb23f15536dc5fbe
Brian J Brennan <brianloveswords@gmail.com>
2014-02-26 02:26:53 -0500

    Initial.

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = altcaps
+function altcaps(words) {
+  const letters = words.toString('utf8')
+  const methods = ['Upper', 'Lower']
+  var idx = 0
+  var midx = (Math.random() * 100)|0
+  var newstring = ''
+  var letter
+  for (; idx < letters.length; idx++) {
+    letter = letters[idx]
+    newstring += (letter.match(/\s/))
+      ? letter
+      : letter['to' + methods[midx++ % 2] + 'Case']()
+  }
+  return newstring
+}
+
+if (!module.parent) {
+  const concat = require('concat-stream')
+  process.stdin.resume()
+  process.stdin.pipe(concat(function (data) {
+    process.stdout.write(altcaps(data.toString()))
+  }))
+}
